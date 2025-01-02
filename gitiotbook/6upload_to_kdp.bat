703e97ab59a23820bf37a038c632cf08bd8f6749
Sheetmet <aldoc@gmail.com>
2025-01-01 18:30:11 -0700

    fix spelling, add stand-alone image for cover

---
+++ gitiotbook/6upload_to_kdp.bat
@@ -0,0 +1,12 @@
+@REM oh yeah you wish, you gotta do this yourself
+
+1. Login to KDP site
+2. Upload the .epub (or kcf) version when prompted, it will upload the file, but then will also
+run a spell checker on it.  If you had spelling errors, you'll want to fix them locally, then
+rerun the converter to epub/kcf format and then upload it again with the fixes.
+
+3. Set up cover if you don't have one or if, updating to a new version/edition, edit the
+existing cover and uplaod any new images for the cover.  It'll pop you into a very basic
+image editor. For the cover, you should have just a basic image with your logo on it and then
+you can add text in the cover editor on KDP.
+4. Follow the rest of the prompts, you should be able to set up pricing and then hit the publish button
